04a6136a4ac382d65ec32f5a237d7a15d1e19e36
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-07-07 16:59:08 +0700

    cm

--- mortgage_repayment/src/App.jsx
+++ mortgage_repayment/src/App.jsx
@@ -7,9 +7,61 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Main/>
+  )
+}
+function Main(){
+  return (
+    <div className='grid grid-cols-2 w-3/4 mx-auto mt-28'>
+      <MortgageCalculator/> 
+      <div className='bg-blue-500'>col2</div>
+    </div>
+  )
+}
+function MortgageCalculator(){
+  const [clickAmoutInp,setClickAmountInp] = useState(false)
 
-    </>
+  return (
+      <div className='bg-red-500 p-8'>
+        <form>
+          <div className='flex justify-between'>
+            <h1 className='font-bold text-lg text-slate-900'>
+              Mortgage Calculator
+            </h1>
+            <button className='underline decoration-dotted text-sm font-light text-slate-300 underline-offset-2'>
+              clear all
+            </button>
+          </div>
+          <label 
+            htmlFor="Amount"
+            className='text-sm text-slate-300'
+          >
+            Mortgage Amount
+          </label>
+          <div 
+            className={
+              clickAmoutInp ? 
+              'border-lime-300 my-2 flex border border-slate-300' 
+              :
+              'my-2 flex border border-slate-300'
+            }
+            
+          >
+            <span className={
+              clickAmoutInp ?
+              'bg-lime-300 p-2 font-bold bg-slate-300'
+              :
+              'p-2 font-bold bg-slate-300'
+            }>￡</span>
+            <input 
+              type="text" 
+              name='Amount' 
+              className='outline-none w-full p-2'
+              onClick={() => setClickAmountInp(true)}
+              onC />
+          </div>
+        </form>
+      </div>
   )
 }
 
