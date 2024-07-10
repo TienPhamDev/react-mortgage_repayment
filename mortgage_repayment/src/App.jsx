7a1965e3c360d721459e7d19b8a16813b7033d66
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-07-10 18:30:12 +0700

    cm

--- mortgage_repayment/src/App.jsx
+++ mortgage_repayment/src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -32,16 +30,35 @@ function MortgageCalculator(){
               clear all
             </button>
           </div>
-          <label 
-            htmlFor="Amount"
-            className='text-sm text-slate-300'
-          >
+          <Label htmlFor="Amount">
             Mortgage Amount
-          </label>
+          </Label>
           <Input inputName="Amount"/>
+          <div className='flex justify-between gap-6'>
+            <div>
+            <Label htmlFor="Term">
+              Mortgage Term
+            </Label>
+            <SmallInput inputName="Term" spanText="Years"/>
+            </div>
+            <div>
+            <Label htmlFor="Interest">
+              Interest Rate
+            </Label>
+            <SmallInput inputName="Interest" spanText="%"/>
+            </div>
+          </div>
         </form>
       </div>
   )
+}
+function Label({children,htmlFor}){
+  return <label 
+          htmlFor={htmlFor}
+          className='text-sm text-slate-300'
+        >
+          {children}
+        </label>
 }
 function Input({inputName}){
   const [clickInp,setClickInp] = useState(false)
@@ -53,22 +70,51 @@ function Input({inputName}){
   return (
     <div 
       className={
-        clickInp ? 'border-lime-400 my-2 flex border-2' : 'my-2 flex border-2 border-slate-400'
+        clickInp ? 'border-lime-400 my-2 flex border-2 rounded-md' : 'my-2 flex border-2 border-slate-400 rounded-md'
       } 
-      >
+    >
       <span className={
         clickInp ?
-        'bg-lime-400 p-2 font-bold'
+        'bg-lime-400 p-2 font-bold rounded-l'
         :
-        'p-2 font-bold bg-slate-300'
+        'p-2 font-bold bg-slate-300 rounded-l'
       }>￡</span>
+      <input 
+        type="text" 
+        id={inputName}
+        className='outline-none w-full p-2 rounded-r'
+        onClick={() => setClickInp(true)}
+      />
+    </div>
+  )
+}
+function SmallInput({inputName,spanText}){
+  const [clickInp,setClickInp] = useState(false)
+  useEffect(()=>{
+    document.addEventListener("click",(e)=>{
+      e.target.id === inputName ? setClickInp(true) : setClickInp(false)
+    })
+  },[])
+  return (
+    <div 
+      className={
+        clickInp ? 'border-lime-400 my-2 flex border-2' : 'my-2 flex border-2 border-slate-400'
+      } 
+    >
       <input 
         type="text" 
         id={inputName}
         className='outline-none w-full p-2'
         onClick={() => setClickInp(true)}
       />
+      <span className={
+        clickInp ?
+        'bg-lime-400 p-2 font-bold w-24 text-center'
+        :
+        'p-2 font-bold bg-slate-300 w-24 text-center'
+      }>{spanText}</span>
     </div>
   )
 }
+
 export default App
