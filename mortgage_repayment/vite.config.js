f4477037a475ad4673c181f90b57a4eece90c6ff
TienPhamDev <manhtienbn18891@gmail.com>
2024-07-21 20:44:44 -0400

    cm

--- mortgage_repayment/vite.config.js
+++ mortgage_repayment/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/mortgage_repayment/",
+  base:"/react-mortgage_repayment/",
   plugins: [react()],
 })
